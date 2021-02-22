5033dde998ad18a3f1f806623b220e4443728956
Ghom <camille.abella@hotmail.fr>
2021-02-22 19:54:12 +0100

    fix last bugs in pagination system

--- src/app/pagination.ts
+++ src/app/pagination.ts
@@ -13,7 +13,7 @@ export class Paginator extends Events.EventEmitter {
   static paginations: Paginator[] = []
 
   private pageIndex = 0
-  private deactivation: NodeJS.Timeout
+  private deactivation?: NodeJS.Timeout
   messageID: string | undefined
   emojis: PaginatorEmojis = {
     previous: "◀️",
@@ -123,14 +123,14 @@ export class Paginator extends Events.EventEmitter {
   }
 
   private resetDeactivationTimeout() {
-    clearTimeout(this.deactivation)
+    clearTimeout(this.deactivation as NodeJS.Timeout)
     return setTimeout(() => this.deactivate().catch(), this.idlTime)
   }
 
   public async deactivate() {
     if (!this.messageID) return
 
-    clearTimeout(this.deactivation)
+    clearTimeout(this.deactivation as NodeJS.Timeout)
 
     // remove reactions if message is not deleted
     const message = await this.channel.messages.cache.get(this.messageID)
