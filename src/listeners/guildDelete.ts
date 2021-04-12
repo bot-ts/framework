fd9582b891b44ee43c92be3d42f5e45ce006ef5b
CamilleAbella <camille.abella@hotmail.fr>
2021-04-12 22:39:01 +0200

    delete guild data on guild delete

---
+++ src/listeners/guildDelete.ts
@@ -0,0 +1,12 @@
+import * as app from "../app"
+
+import guilds from "../tables/guilds"
+
+const listener: app.Listener<"guildDelete"> = {
+  event: "guildDelete",
+  async run(guild) {
+    await guilds.query.delete().where("id", guild.id)
+  },
+}
+
+module.exports = listener
