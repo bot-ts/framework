18283549fa9063bdbadf87a6b35f70b68681c31a
CamilleAbella <camille.abella@hotmail.fr>
2021-05-02 23:17:10 +0200

    start remake of terminal command using events and cache

--- src/commands/terminal.ts
+++ src/commands/terminal.ts
@@ -1,6 +1,16 @@
 import * as app from "../app"
 import cp from "child_process"
 
+interface Process {
+  logs: string[]
+  process: cp.ChildProcess
+  lastUpdate: number
+}
+
+const processes = new Map<number, Process>()
+
+const logsUpdateInterval = 2000
+
 const command: app.Command = {
   name: "terminal",
   aliases: ["term", "cmd", "command", "exec", ">", "process", "shell"],
@@ -12,33 +22,128 @@ const command: app.Command = {
     description: "The cmd to run",
     required: true,
   },
+  flags: [
+    {
+      name: "muted",
+      aliases: ["mute", "silent"],
+      flag: "m",
+      description: "Prevent logs",
+    },
+  ],
   async run(message) {
     message.triggerCoolDown()
 
-    const toEdit = await message.channel.send(
-      new app.MessageEmbed().setTitle("The process is running...")
-    )
+    const current: Process = {
+      logs: [],
+      lastUpdate: Date.now(),
+      process: cp.exec(message.args.cmd, { cwd: process.cwd() }),
+    }
 
-    cp.exec(message.rest, { cwd: process.cwd() }, (err, stdout, stderr) => {
-      const embed = new app.MessageEmbed()
-        .setTitle(
-          err ? "\\❌ An error has occurred." : "\\✔ Successfully executed."
+    const embed = new app.MessageEmbed()
+      .setTitle(
+        `\`[${current.process.pid}]\` The process is ${
+          message.args.muted ? "started" : "running"
+        }.`
+      )
+      .setColor("BLURPLE")
+
+    const toEdit = await message.channel.send(embed)
+
+    const sendLogs = () => {
+      toEdit
+        .edit(
+          embed.setDescription(
+            app.code.stringify({
+              content: current.logs.join("\n").trim() || "No log",
+            })
+          )
         )
-        .setDescription(
-          app.code.stringify({
-            content:
-              (err ? err.stack ?? err.message ?? stderr : stdout).slice(
-                0,
-                2000
-              ) || "No log",
-          })
+        .catch(() => {
+          message.channel.send(embed).catch()
+        })
+    }
+
+    const addLog = (data: any) => {
+      current.logs.push(String(data))
+
+      while (current.logs.join("\n").length > 2000) current.logs.shift()
+
+      if (
+        !message.args.muted &&
+        Date.now() - current.lastUpdate > logsUpdateInterval
+      ) {
+        current.lastUpdate = Date.now()
+
+        sendLogs()
+      }
+    }
+
+    current.process.stdout?.on("data", addLog)
+    current.process.stderr?.on("data", addLog)
+
+    current.process.on("error", (error) => {
+      console.log("error")
+
+      if (!message.args.muted) {
+        embed.setFooter(
+          "❌ " + (error.name || "") + (error.message || "An error occurred")
         )
 
-      toEdit.edit(embed).catch(() => {
-        message.channel.send(embed).catch()
-      })
+        sendLogs()
+      }
     })
+
+    current.process.on("close", (code) => {
+      processes.delete(current.process.pid)
+      console.log("close")
+
+      if (!message.args.muted) {
+        if (code && code !== 0) embed.setColor("RED")
+
+        embed.setTitle(
+          `\`[${current.process.pid}]\` Process ended with exit code: ${code}`
+        )
+
+        sendLogs()
+      }
+    })
+
+    processes.set(current.process.pid, current)
   },
+  subs: [
+    {
+      name: "kill",
+      aliases: ["shutdown", "down", "teardown", "out", "end"],
+      botOwnerOnly: true,
+      description: "Kill the process",
+      positional: [
+        {
+          name: "pid",
+          description: "Process ID",
+        },
+      ],
+      async run(message) {},
+    },
+    {
+      name: "list",
+      aliases: ["ls", "all"],
+      botOwnerOnly: true,
+      description: "List watched processes",
+      async run(message) {},
+    },
+    {
+      name: "logs",
+      botOwnerOnly: true,
+      description: "The last logs of process",
+      positional: [
+        {
+          name: "pid",
+          description: "Process ID",
+        },
+      ],
+      async run(message) {},
+    },
+  ],
 }
 
 module.exports = command
