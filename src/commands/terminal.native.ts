b921de8e709f3060f9651ff4903fc84f1e7f37d8
CamilleAbella <camille.abella@hotmail.fr>
2021-05-22 17:34:55 +0200

    remove spawn command

--- src/commands/terminal.native.ts
+++ src/commands/terminal.native.ts
@@ -1,8 +1,6 @@
 import * as app from "../app"
 import cp from "child_process"
 
-let sp: cp.ChildProcess | null = null
-
 const command: app.Command = {
   name: "terminal",
   aliases: ["term", "cmd", "command", "exec", ">", "process", "shell"],
@@ -48,94 +46,6 @@ const command: app.Command = {
       })
     })
   },
-  subs: [
-    {
-      name: "spawn",
-      aliases: ["sp", "watch"],
-      description: "Watch shell command",
-      botOwnerOnly: true,
-      coolDown: 5000,
-      rest: {
-        all: true,
-        name: "cmd",
-        description: "The cmd to run",
-        required: true,
-      },
-      async run(message) {
-        message.triggerCoolDown()
-
-        const embed = new app.MessageEmbed().setTitle(
-          "The process is running..."
-        )
-        const toEdit = await message.channel.send(embed)
-
-        const editInterval = 1000
-        const logLines = 20
-        const logs: string[] = []
-        let lastEdit = Date.now()
-
-        const edit = (e = embed) => {
-          toEdit.edit(e).catch(() => message.channel.send(e).catch())
-        }
-
-        const log = (...parts: string[]) => {
-          logs.push(parts.join(" "))
-          if (logs.length > logLines) {
-            logs.pop()
-          }
-          if (Date.now() > lastEdit + editInterval) {
-            lastEdit = Date.now()
-            edit(
-              embed.setDescription(
-                app.code.stringify({
-                  lang: "yml",
-                  content: logs.slice().reverse().join("\n").slice(-1000),
-                })
-              )
-            )
-          }
-        }
-
-        sp = cp.spawn(message.rest, { cwd: process.cwd(), shell: true })
-
-        sp.stdout?.on("data", (data) => log(`${data}`.trim()))
-        sp.stderr?.on("data", (data) => log(`${data}`.trim()))
-
-        sp.on("close", (code) => {
-          sp = null
-          edit(
-            embed
-              .setTitle(
-                code !== null && code > 0
-                  ? "\\❌ An error has occurred."
-                  : "\\✔ Successfully executed."
-              )
-              .setDescription(
-                app.code.stringify({
-                  lang: "yml",
-                  content: logs.slice().reverse().join("\n"),
-                })
-              )
-          )
-        })
-      },
-      subs: [
-        {
-          name: "kill",
-          aliases: ["exit", "stop"],
-          botOwnerOnly: true,
-          description: "Stop the spawned process",
-          async run(message) {
-            return message.channel.send(
-              sp
-                ? `The process is ${sp.kill(0) ? "killed" : "not killed..."}`
-                : "No process running"
-            )
-          },
-        },
-      ],
-    },
-  ],
 }
 
 module.exports = command
