fe25a6d0fcfc3eedb6ad01567643ddd47e77a522
CamilleAbella <camille.abella@hotmail.fr>
2021-06-05 08:49:57 +0200

    remove not finished slash commadn feature from master

--- src/index.ts
+++ src/index.ts
@@ -16,13 +16,15 @@ const client = new Discord.Client()
   try {
     await client.login(process.env.BOT_TOKEN)
 
-    if (!app.isFullClient(client))
-      throw new Error("The Discord client is not full.")
+    if (!app.isFullClient(client)){
+      app.error("The Discord client is not full.", "system")
+      client.destroy()
+      process.exit(1)
+    }
 
     await app.tableHandler.load(client)
     await app.commandHandler.load(client)
     await app.listenerHandler.load(client)
-    await app.slashCommandHandler.load(client)
   } catch (error) {
     app.error(error, "system", true)
   }
