f6611f05b4994be9759a5a03467be03b71f54bb5
CamilleAbella <camille.abella@hotmail.fr>
2021-09-22 17:50:32 +0200

    convert eval.native.ts from CommonJS to ESModules

--- src/commands/eval.native.ts
+++ src/commands/eval.native.ts
@@ -76,12 +76,12 @@ export default new app.Command({
 
     code = `${
       code.includes("app")
-        ? 'const _path = require("path");const _root = process.cwd();const _app_path = _path.join(_root, "dist", "app.js");const app = require(_app_path);'
+        ? 'const _path = await import("path");const _root = process.cwd();const _app_path = _path.join(_root, "dist", "app.js");const app = await import(_app_path);'
         : ""
     } ${
       message.args.packages.length > 0
         ? `const req = {${[...installed]
-            .map((pack) => `"${pack}": require("${pack}")`)
+            .map((pack) => `"${pack}": await import("${pack}")`)
             .join(", ")}};`
         : ""
     } ${code}`
