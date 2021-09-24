9edce9cab60606741e8268d1c60796558a71dab1
CamilleAbella <camille.abella@hotmail.fr>
2021-09-24 16:32:05 +0200

    continue to fix typo on logs

--- src/app/database.ts
+++ src/app/database.ts
@@ -59,15 +59,9 @@ export class Table<Type> {
   async make(): Promise<this> {
     try {
       await db.schema.createTable(this.options.name, this.options.setup)
-      logger.log(
-        `created table ${chalk.blueBright(this.options.name)}`,
-        "database"
-      )
+      logger.log(`created table ${chalk.blueBright(this.options.name)}`)
     } catch (error) {
-      logger.log(
-        `loaded table ${chalk.blueBright(this.options.name)}`,
-        "database"
-      )
+      logger.log(`loaded table ${chalk.blueBright(this.options.name)}`)
     }
     return this
   }
