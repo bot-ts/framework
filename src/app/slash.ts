c718eaf38b2e00954d216bca791e7512aef14caf
Nirbose <nirbose2@gmail.com>
2021-10-31 23:03:56 +0100

    start new slash.ts

--- src/app/slash.ts
+++ src/app/slash.ts
@@ -1,10 +1,40 @@
 import * as discord from "discord.js"
 import * as rest from "@discordjs/rest"
 import * as api from "discord-api-types/v9"
+import * as axios from "axios"
 
 import * as core from "./core.js"
 import * as logger from "./logger.js"
 import * as command from "./command.js"
+
+const apiURL = "https://discord.com/api/v9"
+
+export interface ApplicationCommandOptions {
+  type: number,
+  name: string,
+  description: string,
+  required?: boolean,
+  choices?: string[]|number[],
+  autocomplete?: boolean,
+  options?: ApplicationCommandOptions,
+  channel_types?: number
+}
+
+export interface ApplicationCommand {
+  name: string,
+  description: string,
+  options?: object|ApplicationCommandOptions,
+  default_permission?: boolean,
+  type?: number
+}
+
+export function createSlashCommand(command: ApplicationCommand, appID: string, guildID?: string) {
+  const http = new axios.Axios()
+
+  guildID 
+    ? http.post(apiURL + `/applications/${appID}/guilds/${guildID}/commands`)
+    : http.post(apiURL + `/applications/${appID}/commands`)
+}
 
 export function getRestClient() {
   return new rest.REST({ version: "9" }).setToken(
