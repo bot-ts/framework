67a96e4fb1d40ce87679df1e334ee8bfc7a3ff68
Nirbose <nirbose2@gmail.com>
2021-11-21 12:07:08 +0100

    reply slash command
    Permet de répondre au slash command

--- src/commands/slash.native.ts
+++ src/commands/slash.native.ts
@@ -1,13 +1,13 @@
 import * as app from "../app.js"
 
 export default new app.Command({
-  name: "slash2",
+  name: "slash",
   description: "The slash.native command",
   channelType: "all",
   isSlash: true,
   guildSlash: "781105165754433537",
   async run(message) {
     // todo: code here
-    return message.send("slash.native command is not yet implemented.")
+    return message.reply("slash.native command is not yet implemented.")
   }
 })
