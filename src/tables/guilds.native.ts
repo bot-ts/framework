1b583b45229343907fa863b16a521a0c11cd7949
CamilleAbella <camille.abella@hotmail.fr>
2022-01-16 17:27:32 +0100

    remove migration of guilds table

--- src/tables/guilds.native.ts
+++ src/tables/guilds.native.ts
@@ -8,11 +8,6 @@ export interface Guild {
 export default new app.Table<Guild>({
   name: "guilds",
   description: "Represent a guild",
-  migrations: {
-    1: (table) => {
-      // you can use migrations for add columns to your created tables
-    },
-  },
   setup: (table) => {
     table.string("id").unique()
     table.string("prefix").nullable()
