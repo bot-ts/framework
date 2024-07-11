37af4f908bf98d10ad520c9c8069fd192e376316
GhomKrosmonaute <camille.abella@hotmail.fr>
2024-07-11 12:35:48 +0200

    updated index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -11,7 +11,7 @@ try {
   await app.initPagination()
   await app.checkUpdates()
 
-  app.log("correctly started")
+  app.success("no bugs found")
   // eslint-disable-next-line no-undef
   process.exit(0)
 } catch (error) {
