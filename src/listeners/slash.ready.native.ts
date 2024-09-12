7dca2af6e3309beee8798e049b370942c6f3df53
GhomKrosmonaute <camille.abella@hotmail.fr>
2024-09-12 12:56:08 +0200

    fixed typo

--- src/listeners/slash.ready.native.ts
+++ src/listeners/slash.ready.native.ts
@@ -2,7 +2,8 @@ import * as app from "#app"
 
 const listener: app.Listener<"ready"> = {
   event: "ready",
-  description: "Deploy slash commands everywhere",
+  description: "Deploy slash commands",
+  once: true,
   async run(client) {
     if (app.env.BOT_GUILD)
       return app.registerSlashCommands(client, app.env.BOT_GUILD)
