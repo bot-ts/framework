33a592d28cf957ed1acebcf87560a3eb09fb8925
GhomKrosmonaute <camille.abella@hotmail.fr>
2024-10-22 16:44:46 +0200

    fixed the access before initialization of the pagination button

--- src/buttons/pagination.native.ts
+++ src/buttons/pagination.native.ts
@@ -1,12 +1,15 @@
-import * as app from "#app"
+import * as button from "#src/app/button.ts"
+import type * as pagination from "#src/app/pagination.ts"
 
-export type PaginationButtonParams = [key: app.PaginatorKey]
+export type PaginationButtonParams = [key: pagination.PaginatorKey]
 
-export default new app.Button<PaginationButtonParams>({
+export default new button.Button<PaginationButtonParams>({
   key: "pagination",
   description: "The pagination button",
   builder: (builder) => builder.setLabel("Pagination"),
   async run(interaction, key) {
+    const app = await import("#app")
+
     const paginator = app.Paginator.getByMessage(interaction.message)
 
     if (paginator) return paginator.handleInteraction(interaction, key)
